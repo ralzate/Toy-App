ec5f018440af2360f0795f6ee7f246d21577d329
alejoalzate <alejoalzate12@gmail.com>
2021-02-17 13:32:10 -0500

    Model validations and show with the first micropost of each user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    has_many :microposts 
+
+    validates :name, presence: true
+    validates :email, presence: true
+
 end
